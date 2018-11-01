c40f22bc46e8ad858e8a56d53989ceaff5c1977f
Destin Moulton <destin.moulton@gmail.com>
2018-11-01 11:41:17 -0600

    Added a bookmarkids constants file.

---
+++ src/constants/bookmarkids.js
@@ -0,0 +1,9 @@
+const ids = {
+    root: "root________",
+    menu: "menu________",
+    toolbar: "toolbar_____",
+    unfiled: "unfiled_____",
+    mobile: "mobile______"
+};
+
+export default ids;
