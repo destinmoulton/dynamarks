edb961bd081ae8d809bca1a2d3a0a5531defa5c7
Destin Moulton <destin.moulton@gmail.com>
2018-11-01 17:24:04 -0600

    Modified to remove the jsx requirements.

--- webpack.kernel.config.js
+++ webpack.kernel.config.js
@@ -12,13 +12,13 @@ module.exports = {
     },
     devtool: "source-map",
     resolve: {
-        // Add '.ts' and '.tsx' as resolvable extensions.
+        // Add '.ts' as resolvable extension
         extensions: [".ts", ".js", ".json"]
     },
     module: {
         rules: [
-            // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
-            { test: /\.tsx?$/, loader: "ts-loader" },
+            // All files with a '.ts' will be handled by ts-loader
+            { test: /\.ts$/, loader: "ts-loader" },
 
             // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
             { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
