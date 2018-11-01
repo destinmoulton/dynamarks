a6bf4fe3d4244defee1e14bab035c65a0cd60218
Destin Moulton <destin.moulton@gmail.com>
2018-11-01 17:24:46 -0600

    Added message events to the kernel.

--- src/kernel/kernel.ts
+++ src/kernel/kernel.ts
@@ -6,3 +6,37 @@
  *
  * To be run as a background process in the manifest.json.
  */
+import { has } from "lodash";
+class IKernel {
+    channel: any;
+}
+
+class Kernel extends IKernel {
+    constructor() {
+        super();
+        this.channel = null;
+
+        this.initializeEvents();
+    }
+
+    initializeEvents() {
+        browser.runtime.onConnect.addListener(this.listenForMessages);
+        browser.runtime.onMessage.addListener(this.handleMessage);
+    }
+
+    handleMessage = (msg: any) => {
+        console.log(msg);
+        this.channel.postMessage({ response: "TEST RESPONSE" });
+    };
+
+    listenForMessages = (port: any) => {
+        if (port.name !== "dynamarks") return;
+
+        this.channel = port;
+        this.channel.onMessage.addListener((msg: any) => {
+            this.handleMessage(msg);
+        });
+    };
+}
+
+new Kernel();
