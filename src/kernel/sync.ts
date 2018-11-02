329b23dce0f0e49eb243b6c0896e2de0d6aeaaa3
Destin Moulton <destin.moulton@gmail.com>
2018-11-02 11:53:58 -0600

    Created a sync class.

---
+++ src/kernel/sync.ts
@@ -0,0 +1,11 @@
+interface ISync {}
+
+class Sync implements ISync {
+    constructor() {}
+
+    // Get the local bookmarks
+    async getLocalBookmarks() {}
+
+    // Get the remote bookmarks
+    async getRemoteBookmarks() {}
+}
