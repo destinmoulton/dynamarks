3881000aae6309c50ddf40c6953965c6cbe87a91
Destin Moulton <destin.moulton@gmail.com>
2018-11-02 14:24:39 -0600

    Added dispatching to the status.

--- src/kernel/status.ts
+++ src/kernel/status.ts
@@ -1,14 +1,38 @@
 import * as types from "../common/types";
 
-interface IStatus {
+/**
+ * Container for the kernel status.
+ */
+class Status {
     currentStatus: types.EKernelStatus;
-}
+    dispatcher: types.IDispatcher;
+    messenger: types.IClientMessenger;
 
-class Status implements IStatus {
-    currentStatus: types.EKernelStatus;
-
-    constructor() {
+    constructor(
+        dispatcher: types.IDispatcher,
+        messenger: types.IClientMessenger
+    ) {
         this.currentStatus = types.EKernelStatus.Unconfigured;
+        this.dispatcher = dispatcher;
+        this.messenger = messenger;
+
+        this.dispatcher.subscribe("status", this._handleDispatch);
+    }
+
+    private _handleDispatch(msg: types.IDispatchMessage) {
+        switch (msg.action) {
+            case "get":
+                const message = {
+                    topic: "status",
+                    action: "get.result",
+                    data: this.get()
+                };
+                return this.messenger.send(message);
+        }
+    }
+
+    set(newStatus: types.EKernelStatus) {
+        this.currentStatus = newStatus;
     }
 
     get() {
