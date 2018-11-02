85c576e729ebd139b8c5abf939277494f59a84f3
Destin Moulton <destin.moulton@gmail.com>
2018-11-02 14:25:11 -0600

    Added subscription and dispatch code.

--- src/common/dispatcher.ts
+++ src/common/dispatcher.ts
@@ -1,23 +1,34 @@
+import { has } from "lodash";
+
 import * as types from "./types";
 
 /**
  * Dispatcher is a pseudo publisher (ie pub/sub)
  */
-class Dispatcher extends types.IDispatcher {
+class Dispatcher implements types.IDispatcher {
+    subscriptions: types.TDispatchSubscriptions = {};
+
     constructor() {
-        super();
-
-        this.subscribers = [];
+        this.subscriptions = {};
     }
 
-    subscribe(subscriber: any) {
-        this.subscribers.push(subscriber);
+    subscribe(topic: string, subscriber: types.TDispatchSubscriber) {
+        if (!has(this.subscriptions, topic)) this.subscriptions[topic] = [];
+
+        this.subscriptions[topic].push(subscriber);
     }
 
-    dispatch(msg: types.IMessage) {
-        this.subscribers.forEach(sub => {
-            sub(msg);
-        });
+    dispatch(msg: types.IDispatchMessage) {
+        if (!has(this.subscriptions, msg.topic)) {
+            console.error("dispatch() :: topic does not exist", msg);
+            return;
+        }
+
+        this.subscriptions[msg.topic].forEach(
+            (sub: types.TDispatchSubscriber) => {
+                sub(msg);
+            }
+        );
     }
 }
 
