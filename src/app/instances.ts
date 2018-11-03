33aa34d6c65ce91c8bcd231109054f54a080c65e
Destin Moulton <destin.moulton@gmail.com>
2018-11-03 10:53:14 -0600

    Added settings to the instances.

--- src/app/instances.ts
+++ src/app/instances.ts
@@ -1,7 +1,9 @@
 import KernelMessenger from "./lib/kernelmessenger";
 import Dispatcher from "../common/dispatcher";
+import Settings from "../common/settings";
 
 const dispatcher = new Dispatcher();
 const kernelmessenger = new KernelMessenger(dispatcher);
+const settings = new Settings();
 
-export { dispatcher, kernelmessenger };
+export { dispatcher, kernelmessenger, settings };
