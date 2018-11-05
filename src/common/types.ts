68cbc3857f062d431ccda6b9d6727165c598273c
Destin Moulton <destin.moulton@gmail.com>
2018-11-05 13:01:14 -0700

    Added redux types.

--- src/common/types.ts
+++ src/common/types.ts
@@ -71,3 +71,31 @@ export interface IDynalistDocument {
 //
 // END Dynalist Types
 // ------------------
+
+// -----------
+// Redux Types
+//
+export interface IDispatch {
+    <R>(
+        asyncAction: (dispatch: IDispatch, getState: () => IRootStoreState) => R
+    ): R;
+    <R>(asyncAction: (dispatch: IDispatch) => R): R;
+    // (neverAction: (dispatch: Dispatch, getState: () => GetState) => never): never;
+    (action: object): void;
+    // (action: Thunk): ; // thunks in this app must return a promise
+}
+
+export interface IGetState {
+    (): IRootStoreState;
+}
+
+export interface ISettingsState {
+    [key: string]: any;
+}
+
+export interface IRootStoreState {
+    settingsStore: ISettingsState;
+}
+//
+// END Redux Types
+// ---------------
