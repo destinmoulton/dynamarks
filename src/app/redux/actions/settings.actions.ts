b9f60b89ef014c2791d199541605455e69003914
Destin Moulton <destin.moulton@gmail.com>
2018-11-05 13:21:54 -0700

    Added populate() method for populating settings reducer.

--- src/app/redux/actions/settings.actions.ts
+++ src/app/redux/actions/settings.actions.ts
@@ -1,9 +1,27 @@
+import { isEmpty, values } from "lodash";
+
 import * as ActionTypes from "../actiontypes";
+import * as Types from "../../../common/types";
+import SettingsConstants from "../../../common/constants/settings.constants";
+import { settings } from "../../instances";
 
 export function set(key: string, value: any) {
     return {
         type: ActionTypes.SETTINGS_SET,
         key,
         value
+    };
+}
+
+// Populate the settings from storage
+export function populate() {
+    return (dispatch: Types.IDispatch) => {
+        values(SettingsConstants).forEach((settingName: string) => {
+            settings.get(settingName).then(setting => {
+                if (!isEmpty(setting)) {
+                    dispatch(set(settingName, setting));
+                }
+            });
+        });
     };
 }
