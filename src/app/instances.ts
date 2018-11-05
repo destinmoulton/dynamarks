b0fecadfe3740be554bbb1bc0abf5a8ba83addfb
Destin Moulton <destin.moulton@gmail.com>
2018-11-05 14:37:48 -0700

    Added dynalistapi to the instances.

--- src/app/instances.ts
+++ src/app/instances.ts
@@ -1,9 +1,11 @@
+import DynalistAPI from "../common/api";
 import KernelMessenger from "./lib/kernelmessenger";
 import Dispatcher from "../common/dispatcher";
 import Settings from "../common/settings";
 
 const dispatcher = new Dispatcher();
+const dynalistapi = new DynalistAPI();
 const kernelmessenger = new KernelMessenger(dispatcher);
 const settings = new Settings();
 
-export { dispatcher, kernelmessenger, settings };
+export { dispatcher, dynalistapi, kernelmessenger, settings };
