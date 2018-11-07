31541e5048324938827439f51cbcaff7f8cf77a1
Destin Moulton <destin.moulton@gmail.com>
2018-11-06 17:09:23 -0700

    Added SyncInterval to Settings.

--- src/app/components/tabs/SettingsTab.tsx
+++ src/app/components/tabs/SettingsTab.tsx
@@ -2,12 +2,14 @@ import * as React from "react";
 
 import ChangeDocument from "./Settings/ChangeDocument";
 import SignOut from "./Settings/SignOut";
+import SyncInterval from "./Settings/SyncInterval";
 
 class SettingsTab extends React.Component {
     render() {
         return (
             <div>
                 <ChangeDocument />
+                <SyncInterval />
                 <SignOut />
             </div>
         );
