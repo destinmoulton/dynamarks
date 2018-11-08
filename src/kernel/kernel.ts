3d99f4a4621c995344ba728f885511106f65173a
Destin Moulton <destin.moulton@gmail.com>
2018-11-08 14:19:53 -0700

    Added local instances of dynalistapi, settings, and sync.

--- src/kernel/kernel.ts
+++ src/kernel/kernel.ts
@@ -10,21 +10,28 @@
 import * as types from "../common/types";
 
 import ClientMessenger from "./lib/clientmessenger";
-import LocalBookmarks from "./lib/localbookmarks";
 import Dispatcher from "../common/dispatcher";
+import DynalistAPI from "../common/dynalistapi";
+import LocalBookmarks from "./lib/localbookmarks";
+import Settings from "../common/settings";
 import Status from "./status";
+import Sync from "./sync";
 
 class Kernel {
-    localbookmarks: LocalBookmarks = null;
-    dispatcher: Dispatcher = null;
-    messenger: ClientMessenger = null;
-    status: Status = null;
+    private localbookmarks: LocalBookmarks = null;
+    private dispatcher: Dispatcher = null;
+    private settings: Settings = null;
+    private dynalistapi: DynalistAPI = null;
+    private messenger: ClientMessenger = null;
+    private status: Status = null;
 
     constructor() {
         this.localbookmarks = new LocalBookmarks();
         this.dispatcher = new Dispatcher();
         this.messenger = new ClientMessenger(this.dispatcher);
         this.status = new Status(this.dispatcher, this.messenger);
+        this.settings = new Settings();
+        this.dynalistapi = new DynalistAPI(this.settings);
     }
 }
 
