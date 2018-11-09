5714a4a0dad037d1c2ac17d3ef82af62a51e40c0
Destin Moulton <destin.moulton@gmail.com>
2018-11-09 14:03:42 -0700

    Renamed bookmarkids to folders.constants.

---
+++ src/kernel/constants/folders.constants.ts
@@ -0,0 +1,16 @@
+export const BrowserFolderIDs = {
+    root: "root________",
+    menu: "menu________",
+    toolbar: "toolbar_____",
+    unfiled: "unfiled_____",
+    mobile: "mobile______"
+};
+
+export const DynalistFolderNames = {
+    menu: "Menu",
+    toolbar: "Toolbar",
+    unfiled: "Other Bookmarks",
+    mobile: "Mobile"
+};
+
+export const Keys = ["menu", "toolbar", "unfiled", "mobile"];
