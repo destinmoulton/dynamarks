18ce2d3d317a770277e33f34b015f103bab1968e
Destin Moulton <destin.moulton@gmail.com>
2018-11-10 17:06:14 -0700

    Removed unnecessary instances.

--- src/kernel/kernel.ts
+++ src/kernel/kernel.ts
@@ -10,46 +10,28 @@
 import * as types from "../common/types";
 
 import BrowserEvents from "./lib/browserevents";
-import ClientMessenger from "./lib/clientmessenger";
-import Dispatcher from "../common/dispatcher";
+import Messenger from "../common/messenger";
+
 import DynalistAPI from "../common/dynalistapi";
 import LocalBookmarks from "./lib/localbookmarks";
 import Settings from "../common/settings";
-import Status from "./status";
+
 import Sync from "./sync";
 
 class Kernel {
     private browserevents: BrowserEvents = null;
-    private localbookmarks: LocalBookmarks = null;
-    private dispatcher: Dispatcher = null;
     private settings: Settings = null;
     private dynalistapi: DynalistAPI = null;
-    private messenger: ClientMessenger = null;
-    private status: Status = null;
+    private messenger: Messenger = null;
     private sync: Sync = null;
 
     constructor() {
-        this.localbookmarks = new LocalBookmarks();
-        this.dispatcher = new Dispatcher();
-        this.messenger = new ClientMessenger(this.dispatcher);
-        this.status = new Status(this.dispatcher, this.messenger);
+        this.messenger = new Messenger();
         this.settings = new Settings();
         this.dynalistapi = new DynalistAPI(this.settings);
-        this.sync = new Sync(
-            this.dynalistapi,
-            this.localbookmarks,
-            this.settings
-        );
         this.browserevents = new BrowserEvents();
 
-        // this.sync
-        //     .getRemoteBookmarks()
-        //     .then(bookmarks => {
-        //         console.log(bookmarks);
-        //     })
-        //     .catch(err => {
-        //         console.error(err);
-        //     });
+        this.sync = new Sync(this.dynalistapi, this.messenger, this.settings);
     }
 }
 
