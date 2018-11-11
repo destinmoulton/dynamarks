1f0d203da02073479d670340138e35d03bc55c85
Destin Moulton <destin.moulton@gmail.com>
2018-11-11 11:45:13 -0700

    Moved local instances into children.

--- src/kernel/kernel.ts
+++ src/kernel/kernel.ts
@@ -12,7 +12,6 @@ import * as types from "../common/types";
 import BrowserEvents from "./lib/browserevents";
 import Messenger from "../common/messenger";
 
-import DynalistAPI from "../common/dynalistapi";
 import LocalBookmarks from "./lib/localbookmarks";
 import Settings from "../common/settings";
 
@@ -21,17 +20,15 @@ import Sync from "./sync";
 class Kernel {
     private browserevents: BrowserEvents = null;
     private settings: Settings = null;
-    private dynalistapi: DynalistAPI = null;
     private messenger: Messenger = null;
     private sync: Sync = null;
 
     constructor() {
         this.messenger = new Messenger();
         this.settings = new Settings();
-        this.dynalistapi = new DynalistAPI(this.settings);
         this.browserevents = new BrowserEvents();
 
-        this.sync = new Sync(this.dynalistapi, this.messenger, this.settings);
+        this.sync = new Sync(this.messenger, this.settings);
     }
 }
 
