960c2e1815d83062188c640aec81ad6a19e5b37b
Destin Moulton <destin.moulton@gmail.com>
2018-11-12 16:49:59 -0700

    Added methods to populate the top folders.

--- src/kernel/lib/dynamarksdb.ts
+++ src/kernel/lib/dynamarksdb.ts
@@ -2,7 +2,12 @@
  * The database stored in Dynamarks for meta data
  */
 
-import { cloneDeep } from "lodash";
+import { cloneDeep, keys } from "lodash";
+import { DynalistFolders } from "../constants/folders.constants";
+
+import DynalistAPI from "../../common/dynalistapi";
+import DocumentChanges from "./dynalist/documentchanges";
+import * as Types from "../../common/types";
 
 interface IFolderMap {
     [folder_key: string]: string;
@@ -13,13 +18,53 @@ interface IDynarksDB {
 }
 
 const INITIAL_DB: IDynarksDB = {
-    folderMap: null
+    folderMap: {}
 };
 class DynamarksDB {
-    db: IDynarksDB = cloneDeep(INITIAL_DB);
+    db: IDynarksDB = null;
+    iDynalistAPI: DynalistAPI = null;
+    dbNode: Types.IDynalistNode = null;
 
-    addFolderMap(folder_key: string, node_id: string) {
+    constructor(dynalistapi: DynalistAPI) {
+        this.iDynalistAPI = dynalistapi;
+    }
+
+    public setDB(node: Types.IDynalistNode) {
+        let dbData = cloneDeep(INITIAL_DB);
+        if (this.doesNodeContainDB(node)) {
+            dbData = JSON.parse(node.note);
+        }
+        this.dbNode = node;
+        this.db = dbData;
+    }
+
+    public doesNodeContainDB(dbNode: Types.IDynalistNode) {
+        try {
+            const possibleDB = JSON.parse(dbNode.note);
+            return true;
+        } catch (err) {
+            return false;
+        }
+    }
+
+    public async upload() {
+        const changes = new DocumentChanges();
+        const jsonDB = JSON.stringify(this.db);
+        changes.editNode(this.dbNode.id, this.dbNode.content, jsonDB);
+
+        try {
+            return await this.iDynalistAPI.submitChanges(changes.getChanges());
+        } catch (err) {
+            console.error(err);
+        }
+    }
+
+    public addFolderMap(folder_key: string, node_id: string) {
         this.db.folderMap[folder_key] = node_id;
+    }
+
+    public getTopFolderMap() {
+        return this.db.folderMap;
     }
 }
 
--- src/kernel/lib/remotebookmarks.ts
+++ src/kernel/lib/remotebookmarks.ts
@@ -1,30 +1,84 @@
-import { keys, remove, values } from "lodash";
+import { find, keys, remove, reverse, values } from "lodash";
 
 import { DynalistFolders } from "../constants/folders.constants";
 import DocumentChanges from "./dynalist/documentchanges";
 import DynalistAPI from "../../common/dynalistapi";
 import DynamarksDB from "./dynamarksdb";
+import Settings from "../../common/settings";
 import * as Types from "../../common/types";
+
+interface ITopFoldersMap {
+    [key: string]: Types.IDynalistNode;
+}
 
 class RemoteBookmarks {
     bookmarks: Types.IDynalistNode[];
+    topFoldersMap: ITopFoldersMap = {};
     iDynalistAPI: DynalistAPI = null;
+    iDynamarksDB: DynamarksDB = null;
+    iSettings: Settings = null;
 
-    constructor(dynalistapi: DynalistAPI) {
-        this.iDynalistAPI = dynalistapi;
+    constructor(settings: Settings) {
+        this.iSettings = settings;
+        this.iDynalistAPI = new DynalistAPI(this.iSettings);
+        this.iDynamarksDB = new DynamarksDB(this.iDynalistAPI);
     }
 
     public async populate() {
         try {
             this.bookmarks = await this.iDynalistAPI.getBookmarks();
+            const { doFoldersExist, foldersToCreate } = this.checkTopFolders();
+            if (!doFoldersExist) {
+                await this.createTopFolders(foldersToCreate);
+                await this.populate();
+            } else {
+                const dbNode = this.getSingleByName(DynalistFolders.db);
+                // Verify/instantiate the database
+                const isDB = this.iDynamarksDB.doesNodeContainDB(dbNode);
+
+                this.iDynamarksDB.setDB(dbNode);
+                if (!isDB) {
+                    // The folder ids have not been mapped yet
+                    this.createDBTopFolders();
+                    await this.iDynamarksDB.upload();
+                }
+
+                this.mapTopFoldersFromDB();
+            }
         } catch (err) {
             console.error(
-                "RemoteBookmarks :: populate() :: Error getting the bookmarks."
+                "RemoteBookmarks :: populate() :: Error populating the bookmarks.",
+                err
             );
         }
     }
 
-    private async syncDynamarksFolders() {
+    private mapTopFoldersFromDB() {
+        const dbMap = this.iDynamarksDB.getTopFolderMap();
+        keys(DynalistFolders).forEach(folderKey => {
+            this.topFoldersMap[folderKey] = this.getSingleById(
+                dbMap[folderKey]
+            );
+        });
+        console.log(this.topFoldersMap);
+    }
+
+    private createDBTopFolders() {
+        keys(DynalistFolders).forEach(folderKey => {
+            const folder = this.getSingleByName(DynalistFolders[folderKey]);
+            this.iDynamarksDB.addFolderMap(folderKey, folder.id);
+        });
+    }
+
+    private getSingleByName(name: string): Types.IDynalistNode {
+        return find(this.bookmarks, { content: name });
+    }
+
+    private getSingleById(id: string): Types.IDynalistNode {
+        return find(this.bookmarks, { id });
+    }
+
+    private checkTopFolders() {
         const foldersToCreate = values(DynalistFolders);
         keys(DynalistFolders).forEach(folderKey => {
             const folderName = DynalistFolders[folderKey];
@@ -35,9 +89,17 @@ class RemoteBookmarks {
             });
         });
 
+        return {
+            doFoldersExist: foldersToCreate.length === 0,
+            foldersToCreate
+        };
+    }
+
+    private async createTopFolders(foldersToCreate: string[]) {
         if (foldersToCreate.length > 0) {
             const documentChanges = new DocumentChanges();
-            foldersToCreate.forEach(folder => {
+
+            reverse(foldersToCreate).forEach(folder => {
                 documentChanges.addNode("root", folder);
             });
             console.log(
