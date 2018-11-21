ad8a8f74a6baca074548587f7a1d9cd239c0a50b
Destin Moulton <destin.moulton@gmail.com>
2018-11-20 17:15:37 -0700

    Added local instances.

--- src/kernel/lib/synchronize.ts
+++ src/kernel/lib/synchronize.ts
@@ -1,5 +1,23 @@
+import {
+    BookmarkFolderKeys,
+    BrowserFolderIDs
+} from "../constants/folders.constants";
+import DynamarksDB from "./dynamarksdb";
+import LocalBookmarks from "./localbookmarks";
+import RemoteBookmarks from "./remotebookmarks";
+
 class Synchronize {
-    // Compare the remote and local bookmarks
-    isDifferent() {}
+    private iDynamarksDB: DynamarksDB = null;
+    private iLocalBookmarks: LocalBookmarks = null;
+    private iRemoteBookmarks: RemoteBookmarks = null;
+    constructor(
+        dynamarksdb: DynamarksDB,
+        localbookmarks: LocalBookmarks,
+        remotebookmarks: RemoteBookmarks
+    ) {
+        this.iDynamarksDB = dynamarksdb;
+        this.iLocalBookmarks = localbookmarks;
+        this.iRemoteBookmarks = remotebookmarks;
+    }
 }
 export default Synchronize;
