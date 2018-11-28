80ade948dd4b47f0c3e9750a98865f67ae039cf3
Destin Moulton <destin.moulton@gmail.com>
2018-11-28 09:44:01 -0700

    Created syncevents file.

---
+++ src/kernel/lib/sync/syncevents.ts
@@ -0,0 +1,38 @@
+import Messenger from "../../../common/messenger";
+import * as MessengerActions from "../../../common/constants/messengeractions.constants";
+import Sync from "./sync";
+import SyncOverwrite from "./syncoverwrite";
+import * as Types from "../../../common/types";
+
+class SyncEvents {
+    private iMessenger: Messenger = null;
+    private iSync: Sync = null;
+    private iSyncOverwrite: SyncOverwrite = null;
+
+    constructor(
+        messenger: Messenger,
+        sync: Sync,
+        syncoverwrite: SyncOverwrite
+    ) {
+        this.iMessenger = messenger;
+        this.iSync = sync;
+        this.iSyncOverwrite = syncoverwrite;
+
+        this.iMessenger.subscribe("sync", this.handleDispatch);
+    }
+
+    private handleDispatch = async (packet: Types.IDispatchMessage) => {
+        switch (packet.action) {
+            case MessengerActions.SYNC_OVERWRITE_SERVER:
+                return await this.iSyncOverwrite.overwriteServer();
+            case MessengerActions.SYNC_OVERWRITE_LOCAL:
+                return await this.iSyncOverwrite.overwriteLocal();
+            case MessengerActions.SYNC_SYNCHRONIZE:
+                return await this.iSync.synchronize();
+            default:
+                return;
+        }
+    };
+}
+
+export default SyncEvents;
