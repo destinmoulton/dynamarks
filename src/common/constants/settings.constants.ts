1a64b598f5316a407d8f7f904d009acff0911284
Destin Moulton <destin.moulton@gmail.com>
2018-12-06 12:58:05 -0700

    Added interfaces.

--- src/common/constants/settings.constants.ts
+++ src/common/constants/settings.constants.ts
@@ -1,14 +1,27 @@
-interface ISettings {
-    [key: string]: any;
+interface ISettingKeys {
+    [key: string]: string;
+    token: string;
+    doc: string;
+    interval: string;
+    installationID: string;
 }
-export const SettingKeys: ISettings = {
+
+interface ISettingDefaults {
+    [key: string]: null | number;
+    token: null;
+    doc: null;
+    interval: number;
+    installationID: null;
+}
+
+export const SettingKeys: ISettingKeys = {
     token: "dynalistToken",
     doc: "dynalistBookmarksDocument",
     interval: "synchronizationInterval",
     installationID: "installationID"
 };
 
-export const SettingDefaults: ISettings = {
+export const SettingDefaults: ISettingDefaults = {
     token: null,
     doc: null,
     interval: 15,
